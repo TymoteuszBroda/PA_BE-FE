f08cd3770ba03639b65dc08cfb8762a0e51f203f
TymoteuszBroda <125222091+TymoteuszBroda@users.noreply.github.com>
2025-06-23 03:14:14 +0200

    Ensure DB migrations run at startup

--- PA_BE/Program.cs
+++ PA_BE/Program.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PermAdminAPI.Data;
 
 namespace PermAdminAPI
 {
@@ -13,7 +16,15 @@ namespace PermAdminAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
